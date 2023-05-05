0cf485d9cbadf7c01603b38724c628d2ed90f1e2
SuzanaAraujo <suoaraujo@gmail.com>
2023-05-05 18:23:06 -0300

    Criando um array para armazenar as notas dos alunos

--- src/Semana01/Exercicio.java
+++ src/Semana01/Exercicio.java
@@ -5,20 +5,31 @@ import java.util.Scanner;
 
 public class Exercicio {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner aluno = new Scanner(System.in);
+        Scanner numero = new Scanner(System.in);
 
         ArrayList<String> listaAluno = new ArrayList<>();
+        double [] notas = new double[3];
         String nome = "";
+        int n=0;
 
-        while (!nome.equalsIgnoreCase("FIM")) {
+        while (!nome.equalsIgnoreCase("FIM") && n<notas.length) {
             System.out.println("Digite o nome de um aluno ou escreva FIM para finalizar");
-            nome = scan.nextLine();
+            nome = aluno.nextLine();
+            System.out.println("Digite a nota do aluno");
+            notas[n] = numero.nextDouble();
 
             if (!nome.equalsIgnoreCase("FIM")) {
                 listaAluno.add(nome);
                 System.out.println("Aluno " + nome + " cadastrado com sucesso!");
+
             }
+            n++;
+                    }
+         for (int i=0; i< notas.length; i++){
+            System.out.println("A nota do aluno " + listaAluno.get(i) + " é: " + notas[i]);
         }
-        listaAluno.forEach(System.out::println);
+
+        }
     }
-}
+
